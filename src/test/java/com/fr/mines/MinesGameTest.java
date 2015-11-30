8669f6d56cd5719edb857e3a50dc1bd679e8260d
Diana Ortega <dicaormu@yahoo.com>
2015-11-30 11:43:15 +0100

    remove unused imports

--- src/test/java/com/fr/mines/MinesGameTest.java
+++ src/test/java/com/fr/mines/MinesGameTest.java
@@ -1,9 +1,6 @@
 package com.fr.mines;
 
 import com.fr.mines.domain.Coordinate;
-import com.fr.mines.domain.Grid;
-import com.fr.mines.service.MinesWeeperService;
-import com.fr.mines.strategy.RandomMinesStrategy;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,22 +10,22 @@ public class MinesGameTest {
 
     @Test
     public void should_transform_to_coordinate() {
-        MinesGame game = new MinesGame(3,3,1);
-        Coordinate coordinate=game.transformToCoordinate("1,3");
+        MinesGame game = new MinesGame(3, 3, 1);
+        Coordinate coordinate = game.transformToCoordinate("1,3");
         assertEquals(coordinate.getCoordX(), 1);
         assertEquals(coordinate.getCoordY(), 3);
     }
 
     @Test
     public void should_not_transform_to_coordinate() {
-        MinesGame game = new MinesGame(3,3,1);
-        Coordinate coordinate=game.transformToCoordinate("aaa");
+        MinesGame game = new MinesGame(3, 3, 1);
+        Coordinate coordinate = game.transformToCoordinate("aaa");
         assertNull(coordinate);
     }
 
     @Test
     public void should_get_valid_coordinate() {
-        MinesGame game = new MinesGame(3,3,1);
+        MinesGame game = new MinesGame(3, 3, 1);
         assertTrue(game.isValidCoordinate(new Coordinate(1, 1)));
         assertTrue(game.isValidCoordinate(new Coordinate(1, 2)));
         assertTrue(game.isValidCoordinate(new Coordinate(1, 3)));
@@ -42,7 +39,7 @@ public class MinesGameTest {
 
     @Test
     public void should_get_not_valid_coordinate() {
-        MinesGame game = new MinesGame(3,3,1);
+        MinesGame game = new MinesGame(3, 3, 1);
         assertFalse(game.isValidCoordinate(new Coordinate(0, 0)));
         assertFalse(game.isValidCoordinate(new Coordinate(0, 1)));
         assertFalse(game.isValidCoordinate(new Coordinate(4, 1)));
@@ -50,7 +47,7 @@ public class MinesGameTest {
 
     @Test
     public void should_get_not_valid_coordinate_when_null() {
-        MinesGame game = new MinesGame(3,3,1);
+        MinesGame game = new MinesGame(3, 3, 1);
         assertFalse(game.isValidCoordinate(null));
     }
 }
